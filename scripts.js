6c3a8597b7b485f4117aa204512dfbdc5b32a0d4
VUTCHULA ASHRITH <Ashrith99>
2024-09-09 20:15:12 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function subscribe() {
+    // Replace the URL with the desired link for the subscription
+    window.location.href = "https://forms.gle/uuez9SDqtz8Nz7Vy8";
+}
+
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.style.width === '250px') {
+        mobileMenu.style.width = '0';
+    } else {
+        mobileMenu.style.width = '250px';
+    }
+}
+
